941c09649b2f0ca8f2efc302f038bb23d505ab0a
Mr Pria Mitra Purba <103563763+priamitrapurba@users.noreply.github.com>
2023-06-19 21:57:19 +0700

    Initial commit

---
+++ app/views/index/index.php
@@ -0,0 +1,11 @@
+<h2>Log In</h2>
+
+<div class="info">Selamat datang, <strong>MITRA</strong></div>
+
+<form action="<?php echo URL; ?>/Dashboard/index" method="post">
+    <p>username</p>
+    <input type="text">
+    <p>passwor</p>
+    <input type="password">
+    <input type="submit" value="Login">
+</form>
